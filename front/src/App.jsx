dd06c1d5923a1063443a7a0aed30e311c164b635
Satel14 <ostaplvov@gmail.com>
2022-01-13 15:38:03 +0200

    add front

---
+++ front/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import { Leaderboards } from './pages/Leaderboards';
+import { Singlepage } from './pages/Singlepage';
+import { Notfoundpage } from './pages/Notfoundpage';
+
+import { Layout } from './components/Layout'
+
+import { RequireAuth } from './hoc/RequireAuth'
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="posts" element={<Leaderboards />} />
+          <Route path="posts/:id" element={<Singlepage />} />
+          <Route path="posts/new" element={
+            <RequireAuth>
+            </RequireAuth>
+          } />
+
+          <Route path="*" element={<Notfoundpage />} />
+        </Route>
+      </Routes>
+  );
+}
+
+export default App;
